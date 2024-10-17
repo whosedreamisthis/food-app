bcf736d9483e2ae173cacde5dc54f2a3fdb44d8f
Dana Sharon <whosedreamisthis@gmail.com>
2024-10-17 09:47:14 -0700

    validate form

--- app/meals/share/page.js
+++ app/meals/share/page.js
@@ -1,7 +1,7 @@
 import classes from './page.module.css';
 import ImagePicker from '@/components/meals/image-picker';
 import { shareMeal } from '@/lib/actions';
-
+import MealsFormSubmit from '@/components/meals/meals-form-submit';
 export default function ShareMealPage() {
 	return (
 		<>
@@ -53,7 +53,7 @@ export default function ShareMealPage() {
 					</p>
 					<ImagePicker label="Your image" name="image" />
 					<p className={classes.actions}>
-						<button type="submit">Share Meal</button>
+						<MealsFormSubmit />
 					</p>
 				</form>
 			</main>
---
+++ components/meals/meals-form-submit.js
@@ -0,0 +1,11 @@
+'use client';
+import React from 'react';
+import { useFormStatus } from 'react-dom';
+export default function MealsFormSubmit() {
+	const { pending } = useFormStatus();
+	return (
+		<button type="submit" disabled={pending}>
+			{pending ? 'Submitting' : 'Share Meal'}
+		</button>
+	);
+}
