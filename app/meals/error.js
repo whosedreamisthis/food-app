112cca0f0c0dc0df201d9ccf103a27c212c3bd42
Dana Sharon <whosedreamisthis@gmail.com>
2024-10-16 18:24:06 -0700

    error handling

---
+++ app/meals/error.js
@@ -0,0 +1,11 @@
+'use client';
+import React from 'react';
+
+export default function Error() {
+	return (
+		<main className="error">
+			<h1>An error occured!</h1>
+			<p>Failed to fetch meal data. Please try again later.</p>
+		</main>
+	);
+}
---
+++ app/not-found.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+export default function NotFound() {
+	return (
+		<main className="error">
+			<h1>Not Found</h1>
+			<p>
+				Unfortunately, we could not find the requested page or resource.
+			</p>
+		</main>
+	);
+}
