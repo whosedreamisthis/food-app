57266dccf5eb4fb2ba96deb5cfa70e6d1c790805
Dana Sharon <whosedreamisthis@gmail.com>
2024-10-22 11:33:01 -0700

    still trying to revert to earlier commit

--- app/meals/share/page.js
+++ app/meals/share/page.js
@@ -16,19 +16,7 @@ export default function ShareMealPage() {
 				<p>Or any other meal you feel needs sharing!</p>
 			</header>
 			<main className={classes.main}>
-				{
-					<form action={shareMeal}>
-						<button type="submit">PRESS HERE</button>
-					</form>
-				}
-			</main>
-		</>
-	);
-}
-
-{
-	/*{' '}
-				<form className={classes.form} action={shareMeal}>
+				<form className={classes.form} action={formAction}>
 					<div className={classes.row}>
 						<p>
 							<label htmlFor="name">Your name</label>
@@ -67,10 +55,12 @@ export default function ShareMealPage() {
 						></textarea>
 					</p>
 					<ImagePicker label="Your image" name="image" />
-					{/* {state.message && <p>{state.message}</p>} }
+					{state.message && <p>{state.message}</p>}
 					<p className={classes.actions}>
 						<MealsFormSubmit />
 					</p>
-				</form>{' '}
-				*/
+				</form>
+			</main>
+		</>
+	);
 }
