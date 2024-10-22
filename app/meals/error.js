2ffab46084e5fa6f3c7dbc88cdf6b9c786ffab60
Dana Sharon <whosedreamisthis@gmail.com>
2024-10-22 10:21:13 -0700

    debugging adding more error messaging

--- app/meals/error.js
+++ app/meals/error.js
@@ -5,8 +5,8 @@ export default function Error({ error }) {
 	return (
 		<main className="error">
 			<h1>An error occured!</h1>
-			<p>Failed to fetch meal data. Please try again later.why?</p>
-			<div>{error}</div>
+			<p>Failed to fetch meal data. Please try again later.</p>
+			<p>{error.message}</p>
 		</main>
 	);
 }
