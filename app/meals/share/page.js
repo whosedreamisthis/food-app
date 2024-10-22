746f05f7d70bbf390fb5306bc70c4db4bb265b34
Dana Sharon <whosedreamisthis@gmail.com>
2024-10-22 11:38:27 -0700

    simplify form and shareMeal for debugging purposes

--- app/meals/share/page.js
+++ app/meals/share/page.js
@@ -16,51 +16,55 @@ export default function ShareMealPage() {
 				<p>Or any other meal you feel needs sharing!</p>
 			</header>
 			<main className={classes.main}>
-				<form className={classes.form} action={formAction}>
-					<div className={classes.row}>
-						<p>
-							<label htmlFor="name">Your name</label>
-							<input type="text" id="name" name="name" required />
-						</p>
-						<p>
-							<label htmlFor="email">Your email</label>
-							<input
-								type="email"
-								id="email"
-								name="email"
-								required
-							/>
-						</p>
-					</div>
-					<p>
-						<label htmlFor="title">Title</label>
-						<input type="text" id="title" name="title" required />
-					</p>
-					<p>
-						<label htmlFor="summary">Short Summary</label>
-						<input
-							type="text"
-							id="summary"
-							name="summary"
-							required
-						/>
-					</p>
-					<p>
-						<label htmlFor="instructions">Instructions</label>
-						<textarea
-							id="instructions"
-							name="instructions"
-							rows="10"
-							required
-						></textarea>
-					</p>
-					<ImagePicker label="Your image" name="image" />
-					{state.message && <p>{state.message}</p>}
-					<p className={classes.actions}>
-						<MealsFormSubmit />
-					</p>
+				<form action={shareMeal}>
+					<button type="submit">Click</button>
 				</form>
 			</main>
 		</>
 	);
 }
+
+// <form className={classes.form} action={formAction}>
+// 					<div className={classes.row}>
+// 						<p>
+// 							<label htmlFor="name">Your name</label>
+// 							<input type="text" id="name" name="name" required />
+// 						</p>
+// 						<p>
+// 							<label htmlFor="email">Your email</label>
+// 							<input
+// 								type="email"
+// 								id="email"
+// 								name="email"
+// 								required
+// 							/>
+// 						</p>
+// 					</div>
+// 					<p>
+// 						<label htmlFor="title">Title</label>
+// 						<input type="text" id="title" name="title" required />
+// 					</p>
+// 					<p>
+// 						<label htmlFor="summary">Short Summary</label>
+// 						<input
+// 							type="text"
+// 							id="summary"
+// 							name="summary"
+// 							required
+// 						/>
+// 					</p>
+// 					<p>
+// 						<label htmlFor="instructions">Instructions</label>
+// 						<textarea
+// 							id="instructions"
+// 							name="instructions"
+// 							rows="10"
+// 							required
+// 						></textarea>
+// 					</p>
+// 					<ImagePicker label="Your image" name="image" />
+// 					{state.message && <p>{state.message}</p>}
+// 					<p className={classes.actions}>
+// 						<MealsFormSubmit />
+// 					</p>
+// 				</form>
