51f21426cbe8b907777dffd495517a417f985cd3
Dana Sharon <whosedreamisthis@gmail.com>
2024-10-22 10:16:09 -0700

    debugging thrice

--- app/meals/share/page.js
+++ app/meals/share/page.js
@@ -6,7 +6,6 @@ import MealsFormSubmit from '@/components/meals/meals-form-submit';
 import { useFormState } from 'react-dom';
 export default function ShareMealPage() {
 	const [state, formAction] = useFormState(shareMeal, { message: null });
-
 	return (
 		<>
 			<header className={classes.header}>
