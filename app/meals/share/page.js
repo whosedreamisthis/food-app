3683058a514b677d1ba7cb4996b59df78fbc5ba9
Dana Sharon <whosedreamisthis@gmail.com>
2024-10-22 10:43:24 -0700

    simplify shareMeal while debugging

--- app/meals/share/page.js
+++ app/meals/share/page.js
@@ -3,9 +3,9 @@ import classes from './page.module.css';
 import ImagePicker from '@/components/meals/image-picker';
 import { shareMeal } from '@/lib/actions';
 import MealsFormSubmit from '@/components/meals/meals-form-submit';
-import { useFormState } from 'react-dom';
+//import { useFormState } from 'react-dom';
 export default function ShareMealPage() {
-	const [state, formAction] = useFormState(shareMeal, { message: null });
+	//const [state, formAction] = useFormState(shareMeal, { message: null });
 	return (
 		<>
 			<header className={classes.header}>
@@ -55,7 +55,7 @@ export default function ShareMealPage() {
 						></textarea>
 					</p>
 					<ImagePicker label="Your image" name="image" />
-					{state && state.message && <p>{state.message}</p>}
+					{/* {state.message && <p>{state.message}</p>} */}
 					<p className={classes.actions}>
 						<MealsFormSubmit />
 					</p>
